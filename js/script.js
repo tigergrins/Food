391c32af1dba01d07f4dc0163e2a21462d63a72e
Danila Bokun <d.bokun@yandex.ru>
2020-11-27 13:57:51 +0300

    Lesson_61 Создаем слайдер на сайте, вариант 1

--- js/script.js
+++ js/script.js
@@ -317,4 +317,53 @@ window.addEventListener('DOMContentLoaded', () => {
     fetch('http://localhost:3000/menu')
         .then(data => data.json())
         .then(res => console.log(res));
+
+    // Slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current');  
+    let slideIndex = 1;
+
+          showSlides(slideIndex);
+
+          if (slides.length < 10) {
+              total.textContent = `0${slides.length}`;
+          } else {
+              total.textContent = slides.length;
+          }
+
+          function showSlides(n) {
+              if (n > slides.length) {
+                slideIndex = 1;
+              }
+
+              if (n < 1) {
+                slideIndex = slides.length;
+              }
+
+              slides.forEach(item => item.style.display = 'none');
+
+              slides[slideIndex - 1].style.display = 'block';
+
+              if (slideIndex < 10) {
+                current.textContent = `0${slideIndex}`;
+            } else {
+                current.textContent = slideIndex;
+            }
+          }
+
+          function plusSledes(n) {
+              showSlides(slideIndex += n);
+          }
+
+          prev.addEventListener('click', () => {
+            plusSledes(-1);
+          });
+
+          next.addEventListener('click', () => {
+            plusSledes(1);
+          });
 });
